7131b08f18a60942ab0acc65cc2d7160d96a04b3
Chris Norman <chris.norman2@googlemail.com>
2021-04-25 18:33:32 +0100

    Make the first argument to `Terrain` be of type `Game`.

--- lib/src/mapping/terrain.dart
+++ lib/src/mapping/terrain.dart
@@ -1,7 +1,7 @@
 /// Provides the [Terrain] class.
 import 'dart:io';
 
-import '../sound/audio_factory.dart';
+import '../game.dart';
 import 'zone.dart';
 
 /// A piece of a [Zone].
@@ -9,11 +9,11 @@ import 'zone.dart';
 /// This class can be thought of as a tile on a map.
 class Terrain<T> {
   /// Create some terrain.
-  Terrain(this.factory, this.footstepSound,
+  Terrain(this.game, this.footstepSound,
       {this.ambiance, this.ambianceGain = 0.5, this.data});
 
-  /// The audio factory used by this terrain.
-  final AudioFactory factory;
+  /// The game this terrain is part of.
+  final Game game;
 
   /// The footstep sound which will be heard when walking on this terrain.
   final FileSystemEntity footstepSound;
